000fc4da64c1d50a00c82ae895f545eea0b93616
Sean <sean.yang@econommi.io>
2023-05-08 17:41:53 -0700

    JSON encoding

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 
-	// define routes
+	// define routes (contains default multiplexer)
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
@@ -18,4 +26,16 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ashish", "New Delhi", "110075"},
+		{"Rob", "New Delhi", "110075"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	// encodes all of our customers in JSON format
+	json.NewEncoder(w).Encode(customers)
 }
