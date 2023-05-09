12573f86138bc53ac6fc29e114e298dbbc555348
Sean <sean.yang@econommi.io>
2023-05-08 22:15:57 -0700

    implemented hexagonal architecture

--- app/app.go
+++ app/app.go
@@ -3,6 +3,10 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/seanyang20/banking/domain"
+	"github.com/seanyang20/banking/service"
 )
 
 func Start() {
@@ -13,14 +17,50 @@ func Start() {
 	// // starting server
 	// log.Fatal(http.ListenAndServe("localhost:8000", nil))
 
-	// custom mux
-	mux := http.NewServeMux()
+	// // ------
+	// // custom mux
+	// // -------
+	// mux := http.NewServeMux()
 
+	// // define routes
+	// mux.HandleFunc("/greet", greet)
+	// mux.HandleFunc("/customers", getAllCustomers)
+
+	// // starting server
+	// log.Fatal(http.ListenAndServe("localhost:8000", mux))
+
+	// // ------
+	// // gorilla/ mux
+	// // -------
+	// router := mux.NewRouter()
+	// // define routes
+	// router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	// router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	// router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	// router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	// // starting server
+	// log.Fatal(http.ListenAndServe("localhost:8000", router))
+
+	// ------
+	// implementing hexagonal architecture
+	// -------
+	router := mux.NewRouter()
+	//wiring
+	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	// define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
-
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
+
+// func createCustomer(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Post request received")
+// }
+
+// func getCustomer(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	fmt.Fprint(w, vars["customer_id"])
+// }
--- app/handlers.go
+++ app/handlers.go
@@ -3,8 +3,9 @@ package app
 import (
 	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"net/http"
+
+	"github.com/seanyang20/banking/service"
 )
 
 type Customer struct {
@@ -13,16 +14,22 @@ type Customer struct {
 	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
 
-// ResponseWriter is what we are sending back to client
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+type CustomerHandlers struct {
+	service service.CustomerService
 }
 
-func getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customers := []Customer{
-		{"Ashish", "New Delhi", "110075"},
-		{"Rob", "New Delhi", "110075"},
-	}
+// // ResponseWriter is what we are sending back to client
+// func greet(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World!")
+// }
+
+func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	// customers := []Customer{
+	// 	{"Ashish", "New Delhi", "110075"},
+	// 	{"Rob", "New Delhi", "110075"},
+	// }
+
+	customers, _ := ch.service.GetAllCustomer()
 
 	// Response header
 	w.Header().Add("Content-Type", "application/json")
