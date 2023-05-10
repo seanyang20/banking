656ec897ad002b3f76c52c03a05ed189f9e5a9e1
Sean <sean.yang@econommi.io>
2023-05-10 15:40:44 -0700

    structured logging

--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -2,11 +2,11 @@ package domain
 
 import (
 	"database/sql"
-	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/seanyang20/banking/errs"
+	"github.com/seanyang20/banking/logger"
 )
 
 type CustomerRepositoryDb struct {
@@ -27,7 +27,8 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 
 	// rows, err := d.client.Query(findAllSql)
 	if err != nil {
-		log.Println("Error while querying customers table " + err.Error())
+		// log.Println("Error while querying customers table " + err.Error())
+		logger.Error("Error while querying customers table " + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
 
@@ -37,7 +38,8 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 		var c Customer
 		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
 		if err != nil {
-			log.Println("Error while scanning customers " + err.Error())
+			// log.Println("Error while scanning customers " + err.Error())
+			logger.Error("Error while scanning customers " + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error")
 		}
 		customers = append(customers, c)
@@ -58,7 +60,8 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found") // when is db is up (docker)
 		} else {
-			log.Println("Error while scanning customer " + err.Error())
+			// log.Println("Error while scanning customer " + err.Error())
+			logger.Error("Error while scanning customer " + err.Error())
 			return nil, errs.NewUnexpectedError("Unexpected database error") // when db is down (docker)
 		}
 	}
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/seanyang20/banking/app"
+	"github.com/seanyang20/banking/logger"
 )
 
 func main() {
 
+	//log.Println("Starting our application...")
+	logger.Info("Starting our application...")
 	app.Start()
 
 }
