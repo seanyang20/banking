c2e2f0a78f85512fbe0a0816982ed98e8d387f93
Sean <sean.yang@econommi.io>
2023-05-10 16:45:56 -0700

    DTO

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/seanyang20/banking/errs"
+import (
+	"github.com/seanyang20/banking/dto"
+	"github.com/seanyang20/banking/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -9,6 +12,25 @@ type Customer struct {
 	Zipcode     string
 	DateofBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 type CustomerRepository interface {
